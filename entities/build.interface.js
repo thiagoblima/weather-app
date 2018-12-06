c3d051fa9b3bab5c29c84cd897ace28118a4fe71
Thiago Lima <thitheguy@gmail.com>
2018-12-05 23:09:54 -0200

    git cache cleared

--- entities/build.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * @author: <thitheguy@gmail.com> Thiago Lima
- * @class: BuildDevEntiry
- * @version: 0.1.0
- * @description: env file with sensitive info.
- * @exports: object
- */
-Object.defineProperty(exports, "__esModule", { value: true });
