d62b1e1aa4d7dbbae116a4d1e94f4a682be52d00
Thiago Lima <thitheguy@gmail.com>
2018-12-05 23:05:16 -0200

    A couple of editings and a clean up for a better organization

--- weather-app/app.ts
+++ weather-app/app.ts
@@ -11,29 +11,33 @@ import { CommandLine } from './command-line.tools';
 
 export class Main {
   build_dev;
-  constructor({...attr}) {
-    this.build_dev = new BuildDev({...attr});
+  constructor({ ...attr }) {
+    this.build_dev = new BuildDev({ ...attr });
   }
 
   public initApp() {
 
     const commandLine = new CommandLine();
 
-    var encodedAddress = encodeURIComponent(commandLine.argv.address);
-    var geocodeUrl = `${this.build_dev.apikeys.googleAPIURL}json?address=${encodedAddress}&key=${this.build_dev.apikeys.googleAPIKey}`;
+    const encodedAddress = encodeURIComponent(commandLine.argv.address);
+    const geocodeUrl = `${this.build_dev.apikeys.googleAPIURL}json?address=${encodedAddress}&key=${this.build_dev.apikeys.googleAPIKey}`;
 
     axios.get(geocodeUrl).then((response) => {
+
       if (response.data.status === 'ZERO_RESULTS') {
         throw new Error('Unable to find that address.');
       }
+
       console.log('response', response);
-      var lat = response.data.results[0].geometry.location.lat;
-      var lng = response.data.results[0].geometry.location.lng;
-      var weatherUrl = `${this.build_dev.apikeys.forecastIOURL}/${this.build_dev.apikeys.forecastIOKey}${lat},${lng}`;
+      const lat = response.data.results[0].geometry.location.lat;
+      const lng = response.data.results[0].geometry.location.lng;
+      const weatherUrl = `${this.build_dev.apikeys.forecastIOURL}/${this.build_dev.apikeys.forecastIOKey}${lat},${lng}`;
       console.log(response.data.results[0].formatted_address);
+     
       return axios.get(weatherUrl);
+
     }).then((response) => {
-      var temperature = response.data.currently.temperature;
+      let temperature = response.data.currently.temperature;
       var apparentTemperature = response.data.currently.apparentTemperature;
       console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
     }).catch((e) => {
