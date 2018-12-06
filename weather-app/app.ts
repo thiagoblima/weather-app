69eeb102f29b01f0f0e3a6cd53a4444749c80ea0
Thiago Lima <thitheguy@gmail.com>
2018-12-05 22:00:54 -0200

    TypeScript transformation hass been started

--- weather-app/app.ts
+++ weather-app/app.ts
@@ -5,44 +5,61 @@
  * requests. 
  */
 
-const yargs = require('yargs');
-const axios = require('axios');
-const buildDev = require('./environments/build-dev');
+import * as yargs from 'yargs';
+import * as axios from 'axios';
+import { BuildDev } from './environments/build.dev';
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `${buildDev.googleAPIURL}json?address=${encodedAddress}&key=${buildDev.googleAPIKey}`;
-
-axios.get(geocodeUrl).then((response) => {
-  if (response.data.status === 'ZERO_RESULTS') {
-    throw new Error('Unable to find that address.');
+export class Main {
+  build_dev;
+  constructor({...attr}) {
+    this.build_dev = new BuildDev({...attr});
   }
-   console.log('response', response);
-  var lat = response.data.results[0].geometry.location.lat; 
-  var lng = response.data.results[0].geometry.location.lng;
-  var weatherUrl = `${buildDev.forecastIOURL}/${buildDev.forecastIOKey}${lat},${lng}`;
-  console.log(response.data.results[0].formatted_address);
-  return axios.get(weatherUrl);
-}).then((response) => {
-  var temperature = response.data.currently.temperature;
-  var apparentTemperature = response.data.currently.apparentTemperature;
-  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
-}).catch((e) => {
-  if (e.code === 'ENOTFOUND') {
-    console.log('Unable to connect to API servers.');
-  } else {
-    console.log(e.message);
+
+  public initApp() {
+
+    const argv = yargs
+      .options({
+        a: {
+          demand: true,
+          alias: 'address',
+          describe: 'Address to fetch weather for',
+          string: true
+        }
+      })
+      .help()
+      .alias('help', 'h')
+      .argv;
+
+    var encodedAddress = encodeURIComponent(argv.address);
+    var geocodeUrl = `${this.build_dev.apikeys.googleAPIURL}json?address=${encodedAddress}&key=${this.build_dev.apikeys.googleAPIKey}`;
+
+    axios.get(geocodeUrl).then((response) => {
+      if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address.');
+      }
+      console.log('response', response);
+      var lat = response.data.results[0].geometry.location.lat;
+      var lng = response.data.results[0].geometry.location.lng;
+      var weatherUrl = `${this.build_dev.apikeys.forecastIOURL}/${this.build_dev.apikeys.forecastIOKey}${lat},${lng}`;
+      console.log(response.data.results[0].formatted_address);
+      return axios.get(weatherUrl);
+    }).then((response) => {
+      var temperature = response.data.currently.temperature;
+      var apparentTemperature = response.data.currently.apparentTemperature;
+      console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+    }).catch((e) => {
+      if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers.');
+      } else {
+        console.log(e.message);
+      }
+    });
+
   }
-});
+
+}
+
+const main = new Main({});
+
+main.initApp();
+
---
+++ weather-app/environments/build.sample.ts
@@ -0,0 +1,20 @@
+/**
+ * @author: Thiago Lima
+ * @version: 0.1.0
+ * @description: env file with sensitive info.
+ * @exports: object
+ */
+
+import { BuildDevEntity } from '../entities/build.interface';
+ 
+export class BuildDev implements BuildDevEntity {
+    public apikeys;
+    constructor({...apikeys}) {
+        this.apikeys = {
+            googleAPIURL: 'https://maps.googleapis.com/maps/api/geocode/',
+            googleAPIKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+            forecastIOURL: 'https://api.darksky.net/forecast/',
+            forecastIOKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        } || apikeys;
+    }
+}
